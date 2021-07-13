073241d24fdf1e27e39d53bb0bab38a6dc6fb5a9
Tom Elliott <tom.elliott@nyu.edu>
2021-07-13 12:18:12 -0500

    inintial package boilerplate

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oikoumene",
+    version="0.0.1",
+    author="Tom Elliott",
+    author_email="tom.elliott@nyu.edu",
+    description="gazetteer tools",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['airtight'],
+    python_requires='>=3.9.6'
+)
